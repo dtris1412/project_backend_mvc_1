d0ff13e1f8dbfb3f0538f87850828f58af77cee2
dtris <daohuutri04@gmail.com>
2025-02-25 23:54:01 +0700

    CRUD

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,15 @@
+const { render } = require("ejs");
 const conection = require("../config/database");
-const getHomepage = (req, res) => {
-  let user = [];
-  conection.query("select * from Users", function (err, result, fields) {
-    res.render("home.ejs");
-    // user = result;
-    // console.log("check result>>>", result);
-    // console.log("check user>>>: ", user);
-    // res.send(JSON.stringify(user));
-  });
+const {
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+} = require("../service/CRUDservices");
+const { use } = require("../routes/web");
+const getHomepage = async (req, res) => {
+  let results = await getAllUsers();
+  return res.render("home.ejs", { listUsers: results });
 };
 const postCreateUsers = (req, res) => {
   let email = req.body.email;
@@ -25,8 +27,52 @@ const postCreateUsers = (req, res) => {
 const getABC = (req, res) => {
   res.render("sample.ejs");
 };
+const getCreatePage = (req, res) => {
+  res.render("create.ejs");
+};
+const getUpdatePage = async (req, res) => {
+  const userId = req.params.userId;
+  let user = await getUserById(userId);
+  res.render("edit.ejs", { userEdit: user });
+};
+
+const postUpdateUsers = async (req, res) => {
+  let email = req.body.email;
+  let name = req.body.myName;
+  let city = req.body.city;
+  let userId = req.body.userId;
+
+  console.log(
+    "email = ",
+    email,
+    "name = ",
+    name,
+    "city = ",
+    city,
+    "userId: ",
+    userId
+  );
+  await updateUserById(email, name, city, userId);
+  res.redirect("/");
+};
+const postDeleteUsers = async (req, res) => {
+  const userId = req.params.userId;
+  let user = await getUserById(userId);
+  res.render("delete.ejs", { userEdit: user });
+};
+const postHandleRemoveUsers = async (req, res) => {
+  const id = req.body.userId;
+  console.log(id);
+  await deleteUserById(id);
+  res.redirect("/");
+};
 module.exports = {
   getHomepage,
   getABC,
   postCreateUsers,
+  getCreatePage,
+  getUpdatePage,
+  postUpdateUsers,
+  postDeleteUsers,
+  postHandleRemoveUsers,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -3,10 +3,20 @@ const {
   getHomepage,
   getABC,
   postCreateUsers,
+  getCreatePage,
+  getUpdatePage,
+  postUpdateUsers,
+  postDeleteUsers,
+  postHandleRemoveUsers,
 } = require("../controllers/homeController");
 const router = express.Router();
 
 router.get("/", getHomepage);
 router.get("/abc", getABC);
+router.get("/create", getCreatePage);
 router.post("/create_user", postCreateUsers);
+router.get("/update/:userId", getUpdatePage);
+router.post("/update_user", postUpdateUsers);
+router.post("/delete_user/:userId", postDeleteUsers);
+router.post("/delete_user", postHandleRemoveUsers);
 module.exports = router;
